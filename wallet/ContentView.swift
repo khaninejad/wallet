ea453e7940b5801521a22dd29498006b423ee170
Payam Khaninejad <khaninejad@gmail.com>
2021-05-03 23:02:22 +0430

    first init

---
+++ wallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wallet
+//
+//  Created by Payam on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wallet/walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletApp.swift
+//  wallet
+//
+//  Created by Payam on 5/3/21.
+//
+
+import SwiftUI
+
+@main
+struct walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
